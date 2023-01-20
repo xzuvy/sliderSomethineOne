e677c16f02cf8a5ef7c61b618805121d5683f03d
Pete <xzuvy27@gmail.com>
2023-01-20 14:53:48 +0300

    Initial Commit

---
+++ sliderSomethineOne/sliderSomethineOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  sliderSomethineOneApp.swift
+//  sliderSomethineOne
+//
+//  Created by Peter Clarke on 20.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct sliderSomethineOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
